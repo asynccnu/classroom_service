e9d734462cb8d880fe81e09e838ce9f258addcfb
neo1218 <neo1218@yeah.net>
2017-05-05 02:27:38 +0800

    for merge

--- service/__init__.py
+++ service/__init__.py
@@ -3,7 +3,8 @@ from flask import Flask
 
 def create_app(config_name='default'):
     app = Flask(__name__)
-    from api import api
+
+    from .api import api
     app.register_blueprint(api, url_prefix='/api')
 
     return app
